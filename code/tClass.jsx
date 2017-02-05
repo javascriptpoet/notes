06b2173f3441d8d188c57829cd73c0a6b31822fe
javascriptpoet <skihappy17@gmail.com>
2017-02-05 13:56:26 -0800

    Update tClass.jsx

--- code/tClass.jsx
+++ code/tClass.jsx
@@ -16,4 +16,22 @@
     );
     const isMyInstance=(c)=>{
         t.struct(instanceProps).is(c);
-        if(extendeeType)extendeeType.is
+        if(extendeeType)extendeeType.isMyInstance(c)
+    };
+    const checkMyMethods=(proto)=>{
+        if(!proto) return false;
+        if(!t.struct(methods,{strict:true}).is(_.omit(proto,'constructor'))) return false;
+        if(extendeeType) return extendeeType.checkMyMethods(Object.getPrototypeOf(proto));
+        if(extendeeClass) return extendeeClass===proto.constructor
+    };
+    const of=(C)=>{
+        myType.is(C);
+        return class extends C {
+            constructor(props){
+                super(t.struct(constructorProps)(props));
+                myType.isMyInstance(this)
+            }
+        }
+    };
+    return myType;
+}`
